eb29c8b54ac6c5a884c511a1a8a2b3267d5e90a1
Gabriel Trabasso <gabr.j.t@gmail.com>
2025-01-14 18:17:59 -0300

    Simplify list entry setup

--- Source/UserInterface/Public/FriendListViewEntryWidget.h
+++ Source/UserInterface/Public/FriendListViewEntryWidget.h
@@ -1,6 +1,5 @@
 ﻿#pragma once
 
-#include "FriendViewModel.h"
 #include "Blueprint/IUserObjectListEntry.h"
 #include "Blueprint/UserWidget.h"
 #include "FriendListViewEntryWidget.generated.h"
@@ -12,23 +11,10 @@ class USERINTERFACE_API UFriendListViewEntryWidget : public UUserWidget, public 
 {
 	GENERATED_BODY()
 
-public:
-	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
-
-	// Optional: Handle selection changes
-	virtual void NativeOnItemSelectionChanged(bool bIsSelected) override;
-
-	// Optional: Handle entry expansion
-	virtual void NativeOnItemExpansionChanged(bool bIsExpanded) override;
-
-protected:
 	// Your entry widget properties
-	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget, AllowPrivateAccess))
 	UTextBlock* NicknameText;
 
-	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget, AllowPrivateAccess))
 	UTextBlock* IsConnectedText;
-
-	UPROPERTY()
-	UFriendViewModel* ViewModel;
 };
