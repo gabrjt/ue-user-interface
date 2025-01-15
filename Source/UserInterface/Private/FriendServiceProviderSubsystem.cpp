d3a3720718387c74bdf008cf504b244eb6d53c94
Gabriel Trabasso <gabr.j.t@gmail.com>
2025-01-14 22:13:54 -0300

    Implement friend service provider subsystem

---
+++ Source/UserInterface/Private/FriendServiceProviderSubsystem.cpp
@@ -0,0 +1,39 @@
+﻿// FriendServiceProviderSubsystem.cpp
+#include "FriendServiceProviderSubsystem.h"
+#include "FriendSubsystem.h"
+
+void UFriendServiceProviderSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	SetFriendServiceImplementation(FriendServiceClass);
+}
+
+void UFriendServiceProviderSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+TScriptInterface<IFriendService> UFriendServiceProviderSubsystem::GetFriendService() const
+{
+	const UGameInstance*    GameInstance { GetGameInstance() };
+	UGameInstanceSubsystem* Subsystem { GameInstance->GetSubsystemBase(FriendServiceClass.ResolveClass()) };
+
+	TScriptInterface<IFriendService> Interface;
+	Interface.SetObject(Subsystem);
+	Interface.SetInterface(CastChecked<IFriendService>(Subsystem));
+
+	return Interface;
+}
+
+void UFriendServiceProviderSubsystem::SetFriendServiceImplementation(FSoftClassPath InFriendServiceClass)
+{
+	if (!ensure(InFriendServiceClass.TryLoadClass<UGameInstanceSubsystem>()))
+	{
+		SetFriendServiceImplementation(UFriendSubsystem::StaticClass());
+
+		return;
+	}
+
+	FriendServiceClass = InFriendServiceClass;
+}
---
+++ Source/UserInterface/Public/FriendServiceProviderSubsystem.h
@@ -0,0 +1,28 @@
+﻿// FriendServiceProviderSubsystem.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FriendService.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "FriendServiceProviderSubsystem.generated.h"
+
+UCLASS(Config=Game)
+class USERINTERFACE_API UFriendServiceProviderSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+	UPROPERTY(Config)
+	FSoftClassPath FriendServiceClass;
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	virtual void Deinitialize() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Friends")
+	TScriptInterface<IFriendService> GetFriendService() const;
+
+	// Optional: Allow runtime switching of implementation
+	UFUNCTION(BlueprintCallable, Category = "Friends")
+	void SetFriendServiceImplementation(FSoftClassPath InFriendServiceClass);
+};
