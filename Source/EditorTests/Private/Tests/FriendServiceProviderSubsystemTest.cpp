a48caad216731478e0f93d81e11bce9e275b9ce3
Gabriel Trabasso <gabr.j.t@gmail.com>
2025-01-15 11:23:39 -0300

    Add friend service provider tests

---
+++ Source/EditorTests/Private/Tests/FriendServiceProviderSubsystemTest.cpp
@@ -0,0 +1,21 @@
+﻿#include "FriendServiceProviderSubsystem.h"
+#include "FriendSubsystem.h"
+#include "SubsystemTestHelper.h"
+
+class FFriendServiceProviderSubsystemTestHelper : public TSubsystemTestHelper<UFriendServiceProviderSubsystem>
+{
+};
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FriendServiceProviderSubsystemGetInterfaceTest,
+	"UserProject.Editor.FriendServiceProviderSubsystem.GetInterface",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
+
+bool FriendServiceProviderSubsystemGetInterfaceTest::RunTest(const FString& Parameters)
+{
+	const FFriendServiceProviderSubsystemTestHelper Helper;
+	const TScriptInterface                          Interface { Helper.Subsystem->GetFriendService() };
+
+	TestEqual("Friend Service returned successfully", Interface.GetObject(), Cast<UObject>(Helper.GameInstance->GetSubsystem<UFriendSubsystem>()));
+
+	return true;
+}
