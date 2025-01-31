c1abe9f1f6c46515a72187d75f5f775b937e6f61
Gabriel Trabasso <gabr.j.t@gmail.com>
2025-01-30 22:15:12 -0300

    Add more tests

--- Source/EditorTests/Private/Tests/FriendsViewModelTest.cpp
+++ Source/EditorTests/Private/Tests/FriendsViewModelTest.cpp
@@ -29,14 +29,14 @@ bool FFriendsViewModelAddToCollectionTest::RunTest(const FString& Parameters)
 
 	UFriendsViewModel* ViewModel { NewObject<UFriendsViewModel>() };
 
-	TestTrue("View Model is Added to Collection",
+	TestTrue("View Model is added to Collection",
 		Helper.Subsystem->GetViewModelCollection()->AddViewModelInstance({
 				UFriendsViewModel::StaticClass(),
 				"ConnectedFriendsViewModel"
 			},
 			ViewModel));
 
-	TestEqual("View Model is Found in Collection",
+	TestEqual("View Model is found in Collection",
 		Cast<UFriendsViewModel>(
 			Helper.Subsystem->GetViewModelCollection()->FindFirstViewModelInstanceOfType(
 				UFriendsViewModel::StaticClass())),
@@ -45,11 +45,52 @@ bool FFriendsViewModelAddToCollectionTest::RunTest(const FString& Parameters)
 	return true;
 }
 
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FFriendsViewModelSubscribeToFriendService,
-	"UserProject.Editor.FriendsViewModel.SubscribeToFriendService",
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FFriendsViewModelPopulate,
+	"UserProject.Editor.FriendsViewModel.Populate",
 	EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
 
-bool FFriendsViewModelSubscribeToFriendService::RunTest(const FString& Parameters)
+bool FFriendsViewModelPopulate::RunTest(const FString& Parameters)
+{
+	const FMVVMGameSubsystemTestHelper Helper;
+
+	UFriendsViewModel* ConnectedFriendsViewModel { NewObject<UFriendsViewModel>(Helper.World) };
+	ConnectedFriendsViewModel->SubscribeFriendsService(EFriendsViewModelType::Connected);
+
+	UFriendsViewModel* DisconnectedFriendsViewModel { NewObject<UFriendsViewModel>(Helper.World) };
+	DisconnectedFriendsViewModel->SubscribeFriendsService(EFriendsViewModelType::Disconnected);
+
+	UFriendsViewModel* FriendsViewModel { NewObject<UFriendsViewModel>(Helper.World) };
+	FriendsViewModel->SubscribeFriendsService(EFriendsViewModelType::Connected | EFriendsViewModelType::Disconnected);
+
+	TestEqual("No Connected Friend is loaded yet", ConnectedFriendsViewModel->GetFriendsCount(), 0);
+	TestEqual("No Disconnected Friend is loaded yet", DisconnectedFriendsViewModel->GetFriendsCount(), 0);
+	TestEqual("No Friends are loaded yet", FriendsViewModel->GetFriendsCount(), 0);
+
+	Helper.LoadFriends();
+
+	const UFriendSubsystem* FriendSubsystem { Helper.GameInstance->GetSubsystem<UFriendSubsystem>() };
+
+	TestEqual("Connected Friend is loaded", ConnectedFriendsViewModel->GetFriendsCount(), 1);
+	TestEqual("Connected Friends are populated",
+		ConnectedFriendsViewModel->GetFriendsCount(),
+		FriendSubsystem->GetConnectedFriends_Implementation().Num());
+
+	TestEqual("Disconnected Friend is loaded", DisconnectedFriendsViewModel->GetFriendsCount(), 1);
+	TestEqual("Disconnected Friends are populated",
+		DisconnectedFriendsViewModel->GetFriendsCount(),
+		FriendSubsystem->GetDisconnectedFriends_Implementation().Num());
+
+	TestEqual("All Friends are loaded", FriendsViewModel->GetFriendsCount(), 2);
+	TestEqual("All Friends are populated", FriendsViewModel->GetFriendsCount(), FriendSubsystem->GetFriendsRef().Num());
+
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FFriendsViewModelUpdate,
+	"UserProject.Editor.FriendsViewModel.Update",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
+
+bool FFriendsViewModelUpdate::RunTest(const FString& Parameters)
 {
 	const FMVVMGameSubsystemTestHelper Helper;
 
@@ -64,214 +105,43 @@ bool FFriendsViewModelSubscribeToFriendService::RunTest(const FString& Parameter
 
 	Helper.LoadFriends();
 
-	TestEqual("Connected Friend is Added", ConnectedFriendsViewModel->GetFriendsCount(), 1);
-	TestEqual("Connected Friend is Added",
-		ConnectedFriendsViewModel->GetFriendsCount(),
-		Helper.GameInstance->GetSubsystem<UFriendSubsystem>()->GetConnectedFriends_Implementation().Num());
-
-	TestEqual("Disconnected Friend is Added", DisconnectedFriendsViewModel->GetFriendsCount(), 1);
-	TestEqual("Disconnected Friend is Added",
-		DisconnectedFriendsViewModel->GetFriendsCount(),
-		Helper.GameInstance->GetSubsystem<UFriendSubsystem>()->GetDisconnectedFriends_Implementation().Num());
-
-	TestEqual("All Friends are Added", FriendsViewModel->GetFriendsCount(), 2);
-	TestEqual("All Friends are Added",
-		FriendsViewModel->GetFriendsCount(),
-		Helper.GameInstance->GetSubsystem<UFriendSubsystem>()->GetFriendsRef().Num());
-
-	return true;
-}
-
-/*
-#include "FriendService.h"
-#include "FriendSubsystem.h"
-#include "FriendsViewModel.h"
-#include "FriendsViewModelSubsystem.h"
-#include "FriendViewModel.h"
-#include "SubsystemTestHelper.h"
-
-class FFriendViewModelSubsystemTestHelper : public TSubsystemTestHelper<UFriendsViewModelSubsystem>
-{
-public:
-	void LoadFriends() const
-	{
-		UDataTable* FriendsTable { NewObject<UDataTable>() };
-		FriendsTable->RowStruct = FFriendData::StaticStruct();
-		FriendsTable->AddRow(FName("Row1"), FFriendData { "TestUser1", "User One", false });
-		FriendsTable->AddRow(FName("Row2"), FFriendData { "TestUser2", "User Two", true });
-
-		GameInstance->GetSubsystem<UFriendSubsystem>()->LoadFriends(FriendsTable);
-	}
-};
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FFriendViewModelSubsystemInitializeTest,
-	"UserProject.Editor.FriendViewModelSubsystem.InitializeViewModels",
-	EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
-
-bool FFriendViewModelSubsystemInitializeTest::RunTest(const FString& Parameters)
-{
-	const FFriendViewModelSubsystemTestHelper Helper;
-
-	TestEqual("No friends loaded yet",
-		Helper.Subsystem->GetDisconnectedFriendsViewModel()->GetFriends().Num() + Helper.Subsystem->
-		GetConnectedFriendsViewModel()->GetFriends().Num(),
-		0);
-
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FFriendViewModelSubsystemPopulateViewModelsTest,
-	"UserProject.Editor.FriendViewModelSubsystem.PopulateViewModels",
-	EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
-
-bool FFriendViewModelSubsystemPopulateViewModelsTest::RunTest(const FString& Parameters)
-{
-	const FFriendViewModelSubsystemTestHelper Helper;
-
-	Helper.LoadFriends();
-
-	TestEqual("Disconnected Friends populated",
-		Helper.Subsystem->GetDisconnectedFriendsViewModel()->GetFriends().Num(),
-		1);
-	TestEqual("Connected Friends populated", Helper.Subsystem->GetConnectedFriendsViewModel()->GetFriends().Num(), 1);
-
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FFriendViewModelSubsystemUpdateViewModelsTest,
-	"UserProject.Editor.FriendViewModelSubsystem.UpdateViewModels",
-	EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
-
-bool FFriendViewModelSubsystemUpdateViewModelsTest::RunTest(const FString& Parameters)
-{
-	const FFriendViewModelSubsystemTestHelper Helper;
-	UFriendSubsystem*                         FriendSubsystem { Helper.GameInstance->GetSubsystem<UFriendSubsystem>() };
-
-	Helper.LoadFriends();
-
-	TestEqual("Disconnected Friends populated",
-		Helper.Subsystem->GetDisconnectedFriendsViewModel()->GetFriends().Num(),
-		1);
-	TestEqual("Connected Friends populated", Helper.Subsystem->GetConnectedFriendsViewModel()->GetFriends().Num(), 1);
-
-	FriendSubsystem->SetFriendIsConnected_Implementation("TestUser1", true);
-
-	TestEqual("Disconnected Friends View Model updated",
-		Helper.Subsystem->GetDisconnectedFriendsViewModel()->GetFriends().Num(),
-		0);
-	TestEqual("Connected Friends View Model updated",
-		Helper.Subsystem->GetConnectedFriendsViewModel()->GetFriends().Num(),
-		2);
-
-	FriendSubsystem->SetFriendLevel_Implementation("TestUser1", 2);
-	FriendSubsystem->SetFriendLevel_Implementation("TestUser2", 3);
-
-	const TArray<UFriendViewModel*>& ConnectedFriendsViewModel {
-		Helper.Subsystem->GetConnectedFriendsViewModel()->GetFriends()
-	};
-
-	const int32 TestUser1Index {
-		ConnectedFriendsViewModel.IndexOfByPredicate([](const UFriendViewModel* Friend)
-		{
-			return Friend && *Friend == "TestUser1";
-		})
-	};
-
-	const int32 TestUser2Index {
-		ConnectedFriendsViewModel.IndexOfByPredicate([](const UFriendViewModel* Friend)
-		{
-			return Friend && *Friend == "TestUser2";
-		})
-	};
-
-	TestNotEqual("TestUser1 Index is valid", TestUser1Index, static_cast<int32>(INDEX_NONE));
-	TestNotEqual("TestUser2 Index is valid", TestUser2Index, static_cast<int32>(INDEX_NONE));
-	TestNotEqual("Indexes are different", TestUser1Index, TestUser2Index);
-	TestEqual("TestUser1 Level updated", ConnectedFriendsViewModel[TestUser1Index]->GetLevel(), 2);
-	TestEqual("TestUser2 Level updated", ConnectedFriendsViewModel[TestUser2Index]->GetLevel(), 3);
-
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FFriendViewModelSubsystemViewModelsDataTest,
-	"UserProject.Editor.FriendViewModelSubsystem.ViewModelsDataMatch",
-	EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
-
-bool FFriendViewModelSubsystemViewModelsDataTest::RunTest(const FString& Parameters)
-{
-	const FFriendViewModelSubsystemTestHelper Helper;
-
-	Helper.LoadFriends();
-
-	const UFriendSubsystem* FriendSubsystem { Helper.GameInstance->GetSubsystem<UFriendSubsystem>() };
-
-	const TArray                     ConnectedFriends { FriendSubsystem->GetConnectedFriends_Implementation() };
-	const TArray<UFriendViewModel*>& ConnectedFriendsViewModel {
-		Helper.Subsystem->GetConnectedFriendsViewModel()->GetFriends()
-	};
-
-	TestEqual("Connected Friends count matches", ConnectedFriends.Num(), ConnectedFriendsViewModel.Num());
-
-	for (int32 Index = 0; Index < ConnectedFriends.Num(); Index++)
-	{
-		const FFriendData&      Friend { ConnectedFriends[Index] };
-		const UFriendViewModel* ViewModel { ConnectedFriendsViewModel[Index] };
-
-		TestEqual("UserID matches", ViewModel->GetUserID(), Friend.UserID);
-		TestEqual("Nickname matches", ViewModel->GetNickname(), Friend.Nickname);
-		TestEqual("bIsConnected matches", ViewModel->GetIsConnected(), Friend.bIsConnected);
-	}
-
-	const TArray                     DisconnectedFriends { FriendSubsystem->GetDisconnectedFriends_Implementation() };
-	const TArray<UFriendViewModel*>& DisconnectedFriendsViewModel {
-		Helper.Subsystem->GetDisconnectedFriendsViewModel()->GetFriends()
-	};
-
-	TestEqual("Disconnected Friends count matches", DisconnectedFriends.Num(), DisconnectedFriendsViewModel.Num());
-
-	for (int32 Index = 0; Index < DisconnectedFriends.Num(); Index++)
-	{
-		const FFriendData&      Friend { DisconnectedFriends[Index] };
-		const UFriendViewModel* ViewModel { DisconnectedFriendsViewModel[Index] };
-
-		TestEqual("UserID matches", ViewModel->GetUserID(), Friend.UserID);
-		TestEqual("Nickname matches", ViewModel->GetNickname(), Friend.Nickname);
-		TestEqual("bIsConnected matches", ViewModel->GetIsConnected(), Friend.bIsConnected);
-	}
-
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FFriendViewModelSubsystemViewModelDataIsUpdatedTest,
-	"UserProject.Editor.FriendViewModelSubsystem.ViewModelDataIsUpdated",
-	EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
-
-bool FFriendViewModelSubsystemViewModelDataIsUpdatedTest::RunTest(const FString& Parameters)
-{
-	const FFriendViewModelSubsystemTestHelper Helper;
-
-	Helper.LoadFriends();
-
 	UFriendSubsystem* FriendSubsystem { Helper.GameInstance->GetSubsystem<UFriendSubsystem>() };
 
-	FFriendData Friend;
-	FriendSubsystem->GetFriend_Implementation("TestUser2", Friend);
-	Friend.Nickname = "Foo";
-	FriendSubsystem->UpdateFriend_Implementation(Friend);
+	TestEqual("Connected Friend is initialized",
+		ConnectedFriendsViewModel->GetFriends()[0]->GetUserID(),
+		FriendSubsystem->GetConnectedFriends_Implementation()[0].UserID);
+	TestEqual("Disconnected Friend is initialized",
+		DisconnectedFriendsViewModel->GetFriends()[0]->GetUserID(),
+		FriendSubsystem->GetDisconnectedFriends_Implementation()[0].UserID);
+	TestEqual("All Friends are initialized",
+		FriendsViewModel->GetFriends()[0]->GetUserID(),
+		FriendSubsystem->GetFriendsRef()[0].UserID);
+	TestEqual("All Friends are initialized",
+		FriendsViewModel->GetFriends()[1]->GetUserID(),
+		FriendSubsystem->GetFriendsRef()[1].UserID);
 
-	const TArray                     ConnectedFriends { FriendSubsystem->GetConnectedFriends_Implementation() };
-	const TArray<UFriendViewModel*>& ConnectedFriendsViewModel {
-		Helper.Subsystem->GetConnectedFriendsViewModel()->GetFriends()
-	};
+	FriendSubsystem->SetFriendLevel_Implementation(ConnectedFriendsViewModel->GetFriends()[0]->GetUserID(), 32);
+	FriendSubsystem->SetFriendLevel_Implementation(DisconnectedFriendsViewModel->GetFriends()[0]->GetUserID(), 64);
 
-	Friend = ConnectedFriends[0];
-	const UFriendViewModel* ViewModel { ConnectedFriendsViewModel[0] };
+	TestEqual("Connected Friend is updated", ConnectedFriendsViewModel->GetFriends()[0]->GetLevel(), 32);
+	TestEqual("Connected Friend is updated",
+		ConnectedFriendsViewModel->GetFriends()[0]->GetLevel(),
+		FriendSubsystem->GetConnectedFriends_Implementation()[0].Level);
 
-	TestEqual("UserID matches", ViewModel->GetUserID(), Friend.UserID);
-	TestEqual("Nickname matches", ViewModel->GetNickname(), Friend.Nickname);
-	TestEqual("bIsConnected matches", ViewModel->GetIsConnected(), Friend.bIsConnected);
-	TestEqual("Nickname updates", ViewModel->GetNickname(), "Foo");
+	TestEqual("Disconnected Friend is updated", DisconnectedFriendsViewModel->GetFriends()[0]->GetLevel(), 64);
+	TestEqual("Disconnected Friend is updated",
+		DisconnectedFriendsViewModel->GetFriends()[0]->GetLevel(),
+		FriendSubsystem->GetDisconnectedFriends_Implementation()[0].Level);
+
+	TestEqual("All Friends are updated", FriendsViewModel->GetFriends()[0]->GetLevel(), 64);
+	TestEqual("All Friends are updated", FriendsViewModel->GetFriends()[1]->GetLevel(), 32);
+
+	FriendSubsystem->
+		SetFriendIsConnected_Implementation(ConnectedFriendsViewModel->GetFriends()[0]->GetUserID(), false);
+
+	TestEqual("Connected Friend is updated", ConnectedFriendsViewModel->GetFriends().Num(), 0);
+	TestEqual("Disconnected Friend is updated", DisconnectedFriendsViewModel->GetFriends().Num(), 2);
+	TestEqual("All Friend are updated", FriendsViewModel->GetFriends().Num(), 2);
 
 	return true;
 }
-*/
